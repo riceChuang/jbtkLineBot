9ffb6a1e2d2479fba3f142ff98710228e2d2f7f6
jason_chuang <jason_chuang@paradise-soft.com.tw>
2019-01-30 11:45:20 +0800

    version one

--- main.go
+++ main.go
@@ -1,1 +1,55 @@
-package jbtkLineBot
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/line/line-bot-sdk-go/linebot"
+	"github.com/riceChuang/jbtkLineBot/types"
+)
+
+var bot *linebot.Client
+
+func main() {
+	var err error
+	bot, err = linebot.New(os.Getenv("ChannelSecret"), os.Getenv("ChannelAccessToken"))
+	log.Println("Bot:", bot, " err:", err)
+	http.HandleFunc("/back", callbackHandler)
+	port := os.Getenv("PORT")
+	addr := fmt.Sprintf(":%s", port)
+	http.ListenAndServe(addr, nil)
+}
+
+func callbackHandler(w http.ResponseWriter, r *http.Request) {
+	events, err := bot.ParseRequest(r)
+	config := types.New("app.yml")
+	fmt.Println(config.Images[0])
+	if err != nil {
+		if err == linebot.ErrInvalidSignature {
+			w.WriteHeader(400)
+		} else {
+			w.WriteHeader(500)
+		}
+		return
+	}
+
+	for _, event := range events {
+		if event.Type == linebot.EventTypeMessage {
+			switch message := event.Message.(type) {
+			case *linebot.TextMessage:
+				if message.Text == "抽" {
+					if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewImageMessage("https://i.imgur.com/TK2NkUc.jpg", "https://i.imgur.com/TK2NkUc.jpg")).Do(); err != nil {
+						log.Print(err)
+					}
+				} else if message.Text == "機吧毛" {
+					if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewImageMessage("https://i.imgur.com/khCKl58.jpg", "https://i.imgur.com/khCKl58.jpg")).Do(); err != nil {
+						log.Print(err)
+					}
+				}
+
+			}
+		}
+	}
+}
--- types/config.go
+++ types/config.go
@@ -1,1 +1,43 @@
 package types
+
+import (
+	"flag"
+	"io/ioutil"
+	"log"
+	"os"
+	"path/filepath"
+
+	_yaml "gopkg.in/yaml.v2"
+)
+
+type Configuration struct {
+	Images []string `yaml:"images"`
+}
+
+func New(fileName string) *Configuration {
+	flag.Parse()
+
+	c := Configuration{}
+
+	//read and parse config file
+	rootDirPath, err := filepath.Abs(filepath.Dir(os.Args[0]))
+	if err != nil {
+		log.Fatalf("config: file error: %s", err.Error())
+	}
+
+	configPath := filepath.Join(rootDirPath, fileName)
+	if _, err := os.Stat(configPath); !os.IsNotExist(err) {
+		// config exists
+		file, err := ioutil.ReadFile(configPath)
+		if err != nil {
+			log.Fatalf("config: file error: %s", err.Error())
+		}
+
+		err = _yaml.Unmarshal(file, &c)
+		if err != nil {
+			log.Fatal("config: config error:", err)
+		}
+	}
+
+	return &c
+}
